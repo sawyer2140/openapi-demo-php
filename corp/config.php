3e7572600287a43ad9b7335e9c682bc989c21b8f
sawyer <283311678@qq.com>
2017-10-17 17:18:00 +0800

    初始化

--- corp/config.php
+++ corp/config.php
@@ -2,7 +2,7 @@
 define('DIR_ROOT', dirname(__FILE__).'/');
 define("OAPI_HOST", "https://oapi.dingtalk.com");
 
-define("CORPID", " ");
+define("CORPID", "1234 ");
 define("SECRET", "");
 define("AGENTID", "");//必填，在创建微应用的时候会分配
 
